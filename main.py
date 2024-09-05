af31a18c09106113f70b6e93560afc1ac0badb73
balvasergiy <balvasergiy@yandex.ru>
2024-09-05 11:28:28 +0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+from lessson0 import string
+
+print(9**0.5*5) #1st program
+print(9.99>9.98 and 1000!=1000.1) #2nd program
+print(2*2+2) #3rd program
+print(2*(2+2))
+print((2*2+2)==(2*(2+2)))
+print(float("123.456"%[10]))#4th program
+print(type(int(123.456)))
+print(123.456*10)
+print(int(123.456)/10)
+print(int(123.456)//int(10))
